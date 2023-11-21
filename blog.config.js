e0aed4382c3b11061aa60cc8bd0a72d26773fb84
xulei16638716887 <95666530+xulei16638716887@users.noreply.github.com>
2023-11-21 13:55:17 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,7 +17,7 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '要笑啊的个人网站', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '要笑啊', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '越努力越幸运', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '要笑啊, 博客', // 网站关键词 英文逗号隔开
